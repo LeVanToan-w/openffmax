d84b074614e009a18991616aa5868f56488c135b
LeVanToan-w <tl7378132@gmail.com>
2025-08-22 03:58:35 -0400

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,11 @@
+import os
+
+def main():
+    game = ConnectionAbortedError
+    game.__call__ = "com.dts.freefiremax"
+    if game == "com.dts.freefiremax":
+        os.system("command")
+    else:
+        exit
+    print({game})
+    main()
